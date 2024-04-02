b5bd19fc2c83306783f71c0d111142dd513f3736
YuvrajSharma92 <yuvrajsharma92@gmail.com>
2024-04-02 13:21:32 +0530

    change image

--- src/Components/extra/Sale.jsx
+++ src/Components/extra/Sale.jsx
@@ -4,44 +4,44 @@ export default function Sale() {
   return (
     <>
     <div>
-        <img src="https://www.bunaai.com/cdn/shop/collections/sale.jpg?v=1709093550" alt="" />
+        <img src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043788/sale_skmmou.webp" alt="" />
     </div>
     <h1 className='text-4xl text-red-400 text-center mt-4'>Sale</h1>
     <div className='w-full grid grid-cols-1 place-content-center place-items-start sm:grid-cols-2 '>
          <div className='grid grid-cols-1 place-content-center place-items-center'> 
               <div className='p-5'>
-              <img src="https://www.bunaai.com/cdn/shop/files/celebs.jpg?v=1705506025&width=720" alt="" />
+              <img src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043784/celebs_sie6ul.webp" alt="" />
               </div>
          </div>
           <div className='w-[100%] place-content-start place-items-center grid grid-cols-2'>
           <div className='rounded-lg w-[75%] py-7'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/files/Rang-0698.jpg?v=1693758486&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/files/Rang-0696.jpg?v=1693758487&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043785/Rang-0696_qnihxu.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043785/Rang-0698_k1od8y.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>HAZEL HARMONY DRESS</h1>
                         <p>₹ 3,860.00</p>
                     </div>
                     <div className='rounded-lg w-[75%]'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/indianwearonline-4483.jpg?v=1677236916&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/indianwearonline-4467.jpg?v=1677236916&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043785/indianwearonline-4467_li6xmc.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043784/indianwearonline-4483_qv5v3a.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>PEACH MOONGA SILK KURTA SET</h1>
                         <p>₹ 4,860.00</p>
                     </div>
                     <div className='rounded-lg w-[75%] py-5'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/files/Summerindianwear-02216.jpg?v=1683959887&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/files/Summerindianwear-02195.jpg?v=1683959887&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043783/Summerindianwear-02195_jvpgtc.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043782/Summerindianwear-02216_hboxnt.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>TIMELESS COTTON SALWAR SUIT</h1>
                         <p>₹ 5,860.00</p>
                     </div>
                     <div className='rounded-lg w-[75%] py-5'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/indianwearonline-4449.jpg?v=1677232020&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/indianwearonline-4438.jpg?v=1677232019&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043786/indianwearonline-4438_ziv2te.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043784/indianwearonline-4449_k6tm2o.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>SEHAJ MUKAISH SUIT SET</h1>
                         <p>₹ 4,110.00</p>
@@ -53,32 +53,32 @@ export default function Sale() {
               <div className='w-[100%] place-content-start place-items-center grid grid-cols-2'>
                <div className='rounded-lg w-[75%] py-7'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8981.jpg?v=1682326096&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8912.jpg?v=1682326097&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043786/bahar-8981_yvjrht.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043783/bahar-8912_rzwmzz.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>HAZEL HARMONY DRESS</h1>
                         <p>₹ 5,860.00</p>
                     </div>
                     <div className='rounded-lg w-[75%]'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8457.jpg?v=1682325397&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8434.jpg?v=1682325396&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043782/bahar-8434_spag7h.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043783/bahar-8457_jlopgp.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>GUL FLORAL DRAPED SAREE</h1>
                         <p>₹ 6,860.00</p>
                     </div>
                     <div className='rounded-lg w-[75%] py-5'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8893.jpg?v=1682325622&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8841.jpg?v=1682325622&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043783/bahar-8841_oma6kl.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043783/bahar-8893_wrqwfq.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>TIMELESS COTTON SALWAR SUIT</h1>
                         <p>₹ 8,860.00</p>
                     </div>
                     <div className='rounded-lg w-[75%] py-5'>
                         <div className='relative rounded-lg'>
-                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8614.jpg?v=1682325696&width=360" alt="" />
-                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://www.bunaai.com/cdn/shop/products/bahar-8622.jpg?v=1682325694&width=360" alt="" />
+                            <img className='rounded-lg absolute hover:opacity-0 z-0 top-0 left-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043783/bahar-8622_t63fbm.webp" alt="" />
+                            <img className='rounded-lg hover:opacity-0 z-0 duration-500' src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043782/bahar-8614_iwrvpy.webp" alt="" />
                         </div>
                         <h1 className='text-red-600 font-medium'>SEHAJ MUKAISH SUIT SET</h1>
                         <p>₹ 5,110.00</p>
@@ -86,7 +86,7 @@ export default function Sale() {
           </div>
           <div className='grid grid-cols-1 place-content-center place-items-center'> 
               <div className='p-5'>
-              <img src="https://www.bunaai.com/cdn/shop/files/client_1.jpg?v=1705552818&width=720" alt="" />
+              <img src="https://res.cloudinary.com/dmvzvq5om/image/upload/v1712043784/client_1_ncrr60.webp" alt="" />
               </div>
          </div>
     </div>
